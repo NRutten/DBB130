eac9e98b47bf0802bd0ad3f1f04b46744d84f8bc
NRutten <n.j.e.rutten@student.tue.nl>
2017-12-04 11:40:42 +0100

    Initial commit

---
+++ controllers/mobile/Form1Controller/Form1ControllerActions.js
@@ -0,0 +1,9 @@
+define({
+    /*
+      This is an auto generated file and any modifications to it may result in corruption of the action sequence.
+    */
+    AS_Button_adaa894f7dd34c6da9ab472105d74859: function AS_Button_adaa894f7dd34c6da9ab472105d74859(eventobject) {
+        var self = this;
+        undefined["null"] = null;
+    }
+});
